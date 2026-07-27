af74c726edbcfaca12ae7c9269760f8497308310
Gamit567 <kepler21222@gmail.com>
2026-07-27 17:16:47 +0100

    #2 created the Customer.java file

---
+++ MEMBERSHIP/src/main/java/GYM/MEMBERSHIP/ModelClasses/Customer.java
@@ -0,0 +1,48 @@
+package GYM.MEMBERSHIP.ModelClasses;
+
+public class Customer {
+    String name;
+    int age;
+    String username;
+    String password;
+
+    public Customer(String name, int age, String username, String password){
+        this.name = name;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

dc8b5edca52f82d19f54af397756e547c8ca7a32
Gamit567 <kepler21222@gmail.com>
2026-07-28 19:09:27 +0100

    #3 created membershipService.java

---
+++ MEMBERSHIP/src/main/java/GYM/MEMBERSHIP/Service/MembershipService.java
@@ -0,0 +1,49 @@
+package GYM.MEMBERSHIP.Service;
+
+import GYM.MEMBERSHIP.ModelClasses.Customer;
+import GYM.MEMBERSHIP.ModelClasses.Membership;
+import GYM.MEMBERSHIP.ModelClasses.MembershipEnum;
+import GYM.MEMBERSHIP.Repository.CustomerRepository;
+import GYM.MEMBERSHIP.Repository.MembershipRepository;
+
+public class MembershipService {
+
+    private final MembershipRepository membershipRepository;
+    private final CustomerRepository customerRepository; 
+
+    public MembershipService(MembershipRepository membershipRepository, CustomerRepository customerRepository){
+        this.membershipRepository = membershipRepository;
+        this.customerRepository = customerRepository;
+    }
+
+    public Membership createMembership(long id){
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new RuntimeException("customer not found"));
+
+        Membership membership = new Membership();
+        customer.setMembership(membership);
+
+        membershipRepository.save(membership);
+        return membership;
+    }
+
+    public Membership changeStatus(long id,boolean status){
+        Membership membership = membershipRepository.findById(id).orElseThrow(() -> new RuntimeException("customer not found"));
+        membership.setStatus(status);
+        membershipRepository.save(membership);
+        return membership;
+
+    }
+
+        public Membership changetype(long id,MembershipEnum type){
+        Membership membership = membershipRepository.findById(id).orElseThrow(() -> new RuntimeException("customer not found"));
+        if (membership.isStatus() == false && type != MembershipEnum.none){
+            changeStatus(id, true);
+        }
+        membership.setType(type);
+        
+        membershipRepository.save(membership);
+        return membership;
+
+    }
+    
+}

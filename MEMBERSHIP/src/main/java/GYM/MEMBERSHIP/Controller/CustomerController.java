e948584178beb2928fd8ebf7850fa22a5700d152
Gamit567 <kepler21222@gmail.com>
2026-07-29 14:36:26 +0100

    #4 created two CustomerService.java functions for creating and getting customers

---
+++ MEMBERSHIP/src/main/java/GYM/MEMBERSHIP/Controller/CustomerController.java
@@ -0,0 +1,46 @@
+package GYM.MEMBERSHIP.Controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import GYM.MEMBERSHIP.ModelClasses.Customer;
+import GYM.MEMBERSHIP.Service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/createCustomer")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Map<String, String> params){
+        String name = String.valueOf(params.get("name"));
+        int age = Integer.valueOf(params.get("age"));
+        String username = String.valueOf(params.get("username"));
+        String password = String.valueOf(params.get("password"));
+
+        Customer customer = customerService.createCustomer( name, age, username, password);
+        return ResponseEntity.ok(customer);
+           
+    }
+
+    @GetMapping("/getcustomer")
+    public ResponseEntity<Customer> getCustomer(@RequestBody Map<String, String> params){
+        Long id = Long.valueOf(params.get("id"));
+        Customer customer = customerService.findById(id);
+        return ResponseEntity.ok(customer);
+    }
+
+    
+    
+}

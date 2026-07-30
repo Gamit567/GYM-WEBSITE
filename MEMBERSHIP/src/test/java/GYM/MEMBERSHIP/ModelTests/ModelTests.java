db421b757b9a61fffd1a603272b69926cccfdd12
Gamit567 <kepler21222@gmail.com>
2026-07-30 18:40:01 +0100

    #5 created model tests

---
+++ MEMBERSHIP/src/test/java/GYM/MEMBERSHIP/ModelTests/ModelTests.java
@@ -0,0 +1,50 @@
+package GYM.MEMBERSHIP.ModelTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import GYM.MEMBERSHIP.ModelClasses.Customer;
+import GYM.MEMBERSHIP.ModelClasses.Membership;
+
+public class ModelTests {
+    public Customer customer;
+    public Membership membership;
+
+    @BeforeEach
+    public void setup(){
+        customer = new Customer();
+        membership = new Membership();
+    }
+
+    @Test
+    public void CustomerTest(){
+       
+        customer.setName("user2");
+        customer.setAge(25);
+        customer.setUsername("user2username");
+        customer.setPassword("newpassword");
+        customer.setMembership(membership);
+
+        assertEquals("user2", customer.getName());
+        assertEquals(25, customer.getAge());
+        assertEquals("user2username", customer.getUsername());
+        assertEquals("newpassword", customer.getPassword());
+        assertEquals(membership, customer.getMembership());
+    }
+
+    public void MembershipTest(){
+        Membership membership = new Membership();
+
+        membership.setStatus(true);
+        membership.setDateStarted(LocalDate.now());
+        
+
+
+    }
+
+    
+}

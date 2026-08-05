2bc2502292c32fc35f2c6517ccf9ac4c562fcdf1
Gamit567 <kepler21222@gmail.com>
2026-08-05 16:53:31 +0100

    #7 creating the home pages

---
+++ fontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to MY Portfolio GYM website</h1>
+      <div className="buttons">
+        <button>login</button>
+        <button>sign up</button>
+      </div>
+      <p>
+        this is my model gym website which is connected to a fully functional backend, where users can login and sign up to the website to
+        access a fictional membership.
+      </p>
+    </div>
+  );
+}
+
+export default Home;

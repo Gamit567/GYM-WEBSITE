8394f65fe004888950b7354dd80d8c1343f8e735
Gamit567 <kepler21222@gmail.com>
2026-08-06 16:08:53 +0100

    #8 home page created

--- fontend/src/App.tsx
+++ fontend/src/App.tsx
@@ -1,6 +1,8 @@
 
 import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
--- fontend/src/pages/Home.tsx
+++ fontend/src/pages/Home.tsx
@@ -1,10 +1,15 @@
+import {  useNavigate } from 'react-router-dom';
+import "./Signup.css"
+
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Welcome to MY Portfolio GYM website</h1>
       <div className="buttons">
-        <button>login</button>
-        <button>sign up</button>
+        <button onClick={() => navigate("/login")}>login</button>
+        <button onClick={() => navigate("/Signup")}>sign up</button>
       </div>
       <p>
         this is my model gym website which is connected to a fully functional backend, where users can login and sign up to the website to
